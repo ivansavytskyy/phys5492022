74263dd470831338e0aba84e523cadd1daf73393
erikstacey <71736849+erikstacey@users.noreply.github.com>
2022-06-06 14:25:34 -0600

    will this fix it idk

--- main_code/Controller.py
+++ main_code/Controller.py
@@ -22,7 +22,7 @@ class Controller():
 
     def run(self):
         for module in self.modules:
-            module.update()
+            module.update(module)
             module.print_diagnostic_data()
 
         print(self.modules[self.mod_name_map["CopernicusII-GPS"]].utc)
