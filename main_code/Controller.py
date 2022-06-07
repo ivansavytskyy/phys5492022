616e7f58ff68db441ae7a28ac31c06a4b25bab2e
erikstacey <71736849+erikstacey@users.noreply.github.com>
2022-06-07 10:30:02 -0600

    added test for communications functionality

--- main_code/Controller.py
+++ main_code/Controller.py
@@ -48,7 +48,7 @@ class Controller():
 
         self.update_time()
 
-        current_temp = self.modules[self.mod_name_map["MAX31865-E"]].ct
+        current_temp = string(self.modules[self.mod_name_map["MAX31865-E"]].ct)
         print(f"Trying to send current temp: {current_temp}")
         self.modules[self.mod_name_map["Antenna"]].send(current_temp)
 
